67d66e23c0b201de71ae7898157451a1abab99e7
Leanne Alhussein <Alhussein-L@ulster.ac.uk>
2025-10-07 12:18:39 +0300

    halfway

---
+++ week3practical/Book.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace week3practical
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private string genre;
+        private int iSBN;
+        private int year;
+        private string publisher;
+
+    }
+}
